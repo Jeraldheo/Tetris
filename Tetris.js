ef07eb7665683e7431355e1e774a4326b4285162
Jeraldheo <jeyson.marin@unah.hn>
2022-06-19 10:36:34 -0600

    The tetrominoes do not go out of the board anymore.

--- Tetris.js
+++ Tetris.js
@@ -1,7 +1,7 @@
 //TODO: Stop pieces from falling out of the grid.
 document.addEventListener('DOMContentLoaded', ()=>{
 
-    let first_div = document.querySelector("div")
+    let first_div = document.querySelector(".grid")
     
     const num_colum = 9
     let start_pos = 2
@@ -16,10 +16,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     const square = [[1,2,num_colum+1,num_colum +2]]
 
-    const I_rotation = [[2, num_colum + 2, 2*num_colum + 2, 3*num_colum + 2], [1,2,3,4]]
+    const I_rotations = [[2, num_colum + 2, 2*num_colum + 2, 3*num_colum + 2], [1,2,3,4]]
 
 
-    const tetrominoes = [L_rorations, Z_rotations, T_rotations, square, I_rotation]
+    const tetrominoes = [L_rorations, Z_rotations, T_rotations, square, I_rotations]
 
     let select_piece = Math.floor(Math.random()*tetrominoes.length)
     let current_piece = tetrominoes[select_piece][0]
@@ -28,6 +28,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
     for(let i = 0; i<162; i++)
     {
         first_div.appendChild(document.createElement("div"))
+    }
+
+    for(let i = 0; i<9; i++)
+    {
+        let new_cell = document.createElement("div")
+        
+        first_div.appendChild(new_cell)
+        new_cell.className = "bottom_outside"
+        new_cell.style.border = 0
     }
 
     let squares = Array.from(document.querySelectorAll(".grid div"))
@@ -45,7 +54,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     draw()
     
-    setInterval(moveDown, 1000)
+    setInterval(moveDown, 100)
 
     function moveDown()
     {
@@ -53,9 +62,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
         start_pos+=num_colum
         draw()
         
+        freeze()
 
     }
 
+    function freeze()
+    {
+        if(current_piece.some(index=> squares[start_pos + index + num_colum].classList.contains('bottom_outside')))
+        {
+            current_piece.forEach(index=>squares[start_pos + index].classList.add('bottom_outside'))
+
+            select_piece = Math.floor(Math.random()*tetrominoes.length)
+            current_piece = tetrominoes[select_piece][0]
+            start_pos = 2
+            draw()
+        }
+    }
+
+    
     
     
 
